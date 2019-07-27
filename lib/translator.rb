5c28df1b97dec2920a4f505c3101178f63962a07
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-27 01:32:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
-  emiticon_hash
+  emoticon_hash
 end
 
 def get_japanese_emoticon
